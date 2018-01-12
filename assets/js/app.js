5ba89de22a10d2a10857e4182ed926c7aeec80a0
Rik <r.verbeek@ttoserve.nl>
2018-01-12 11:33:09 +0100

    added routing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,20 @@
 import vue from 'vue';
+import vuerouter from 'vue-router';
 import app from './components/app';
 import axios from 'axios';
 
+vue.use(vuerouter);
+
 window.axios = axios;
+
+const router = new vuerouter({
+    routes: [{
+        path: '/',
+        component: app 
+    }]
+});
 
 new vue({
     el: '#vue-app',
-    components: { app }
+    router
 });
